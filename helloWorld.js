eba3551f88db8411c5092ac2680c138d893074f3
AdrianHwang <boardsky@126.com>
2015-08-30 22:17:52 +0800

    添加基本路由

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,24 @@
 var http = require('http');
 
+function createPage(res, httpcode,text){
+	res.writeHead(httpcode, {'Content-Type': 'text/plain'});
+	res.end(text);
+}
+
 http.createServer(function(req, res){
-	res.writeHead(200,{'Content-Type': 'Text-plain'});
-	res.end('hello world!');
+	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+	switch(path){
+		case '':
+			createPage(res, '200',  "HomePage");
+			break;
+		case '/about':
+			createPage(res, '200', "about me");
+			break;
+		default:
+			createPage(res, '404', "not found");
+			break;
+
+	}
 }).listen(3000);
 
 console.log("程序开始运行!");
