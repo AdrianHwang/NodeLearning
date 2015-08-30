6cd0665855f9c4edcf4d856a956b14aa9afc60d6
AdrianHwang <boardsky@126.com>
2015-08-30 22:50:16 +0800

    设置静态资源服务

--- helloWorld.js
+++ helloWorld.js
@@ -1,21 +1,34 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
 
-function createPage(res, httpcode,text){
-	res.writeHead(httpcode, {'Content-Type': 'text/plain'});
-	res.end(text);
+function serveStaticFild(res, path, contenttype, responsecode){
+	if(!responsecode) responsecode = 200;
+	fs.readFile(__dirname + path, function(err, data){
+		if(err){
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end('500 - Internal Error');
+		} else {
+			res.writeHead(responsecode, contenttype);
+			res.end(data);
+		}
+	})
 }
+
 
 http.createServer(function(req, res){
 	var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
 	switch(path){
 		case '':
-			createPage(res, '200',  "HomePage");
+			serveStaticFild(res, '/public/home.html', 'text/html');
 			break;
 		case '/about':
-			createPage(res, '200', "about me");
+			serveStaticFild(res, '/public/about.html', 'text/html');
+			break;
+		case '/img/logo.jpeg':
+			serveStaticFild(res, '/public/img/logo.jpeg', 'image/jpeg');
 			break;
 		default:
-			createPage(res, '404', "not found");
+			serveStaticFild(res, '/public/notfound.html', 'text/html', 404);
 			break;
 
 	}
