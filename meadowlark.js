d42b90ed3185961dfbb1ad547a280b2e4412d907
AdrianHwang <boardsky@126.com>
2015-08-31 00:31:36 +0800

    使用express路由

--- meadowlark.js
+++ meadowlark.js
@@ -1,33 +1,38 @@
 var express = require('express');
 var app =  express();
 
+
+//加入handlebars模板引擎 注意
+var handlebars = require('express3-handlebars').create({defaultLayout: 'layout'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
 //设置运行端口
 app.set('port', process.env.PORT || 3000);
 
+
 //加上路由 注意如果没有设置状态码, 默认返回的状态码是200
 app.get('/', function(req,res){
-	res.type('text/plain');
-	res.send('这是一个旅游网站');
+	res.render('home');
 })
 
 app.get('/about', function(req, res){
-	res.type('text/plain');
-	res.send('这是关于我页面');
+	res.render('about');
 })
 
 //定制404页面 app.use是express中间件的一种方法
 app.use(function(req, res){
-	res.type('text/plain');
 	res.status(404);
-	res.send('404 - Not Found');
+	res.render('404');
 })
 
 //定制500页面
 app.use(function(err, req, res, next){
 	console.log(err.stack);
-	res.type('text/plain');
+	
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 app.listen(app.get('port'), function(){
